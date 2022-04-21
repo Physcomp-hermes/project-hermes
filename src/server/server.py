daaeab82d391ed43a16fc949596f2b1cae1302b8
YuanchaoJiang <jyc17862957252@gmail.com>
2022-04-21 23:02:11 +0800

    update

--- src/server/server.py
+++ src/server/server.py
@@ -1,24 +1,46 @@
 
 # This is the main file for hosting local server and managing the socket connection
 import socket
+import threading
 
-s = socket.socket()
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
-s.bind(('127.0.0.1', 8090))
-s.listen(0)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
 
-while True:
 
-    client, addr = s.accept()
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
 
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        content = client.recv(32)
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
-        if len(content) == 0:
-            break
 
-        else:
-            print(content)
-
-    print("Closing connection")
-    client.close()
+print("[STARTING] server is starting...")
+start()

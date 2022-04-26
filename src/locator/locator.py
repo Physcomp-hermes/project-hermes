3dbf3703afc9d992c41c85c9e027fd7795ea432a
JamaJ1701 <jamajeong@gmail.com>
2022-04-26 20:28:52 +1000

    Calibration function gone?

--- src/locator/locator.py
+++ src/locator/locator.py
@@ -10,7 +10,7 @@ class Locator:
         self.markerParams = cv2.aruco.DetectorParameters_create()
         self.people_list = people_list
         self.cam = cv2.VideoCapture(0)
-        self.__update_frame()
+        # self.__update_frame()
     
     def __del__(self):
         self.cam.release()
@@ -24,26 +24,27 @@ class Locator:
         (self.corners, self.ids, self.rejected) = cv2.aruco.detectMarkers(self.frame, self.markerDict, parameters=self.markerParams)
     
     def show_markers(self):
+        '''
+        This is like a prototyping function for showing markers
+        '''
         self.__update_frame()
         frame = self.frame.copy()
         cv2.aruco.drawDetectedMarkers(frame, self.corners, self.ids)
-<<<<<<< Updated upstream
-        cv2.imshow('frame', frame)
-=======
         # Marker side length in metres.
         marker_len = 0.05
         rvecs, tvecs, obj = cv2.aruco.estimatePoseSingleMarkers(self.corners, marker_len, self.camera_matrix, self.dist_coeffs)
         
         rod = np.zeros((3,3))
         if np.any(self.ids):
-            print(len(self.ids))
-            # rvecs = rvecs / math.pi * 180
+            # print(len(self.ids))
+            # rvecs = rvecs / math.pi * 180 
             for i in range(0, len(self.ids)):
-                
+                print("ID: ", self.ids[i])
+                print("corners: ", self.corners[i])
                 cv2.Rodrigues(rvecs[i], rod)
-                print("R: {0}", rvecs[i]/ math.pi * 180)
-                print("T: {0}", tvecs[i])
-                print("Rod: ", rod)
+                # print("R: {0}", rvecs[i]/ math.pi * 180)
+                # print("T: {0}", tvecs[i])
+                # print("Rod: ", rod)
                 # X: Red, Y: Green, Z: Blue
                 cv2.drawFrameAxes(frame, self.camera_matrix, self.dist_coeffs, rvecs[i], tvecs[i], 0.1)
         cv2.imshow('frame', frame)
@@ -53,7 +54,6 @@ class Locator:
 
 
     
->>>>>>> Stashed changes
 
 # Get rotation and translation matrix to mpa them inside the coordinate system
 # Use this to draw a line, see if it goes pass the square that was inputted 
